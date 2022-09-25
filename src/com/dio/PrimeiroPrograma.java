827396444e6c50c89e00f792dcb3f013cc6207df
vanubioinfo <vanubio06@gmail.com>
2022-09-25 14:12:58 -0300

    first commit

---
+++ src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Livro livros = new Livro();
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		
+		/* TODO Auto-generated method stub
+		int a = 2;
+		int b = 3;
+		System.out.println("Hello world" + (a + b));*/
+	}
+}
+
+/*
+class livros {
+	private String nome;
+	private String npag;
+	
+}*/
